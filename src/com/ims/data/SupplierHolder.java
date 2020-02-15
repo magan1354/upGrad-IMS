7162c1b378045644880739323f78cbedb21dfb88
Maganjot Singh <Maganjot.Singh@upgrad.local>
2020-02-15 23:58:05 +0530

    Implemented public access

--- src/com/ims/data/SupplierHolder.java
+++ src/com/ims/data/SupplierHolder.java
@@ -3,5 +3,16 @@ package com.ims.data;
 import com.ims.actor.Supplier;
 
 class SupplierHolder {
+    Supplier[] suppliers = new Supplier[10000];
+    int currentIndex = 0;
 
+    // Adding/Storing a supplier and return id of Supplier
+    int saveSupplier(Supplier supplier){
+        suppliers[currentIndex]=supplier;
+        currentIndex = currentIndex +1;
+        return currentIndex;
+    }
+
+
+    // Fetching a supplier with a particular index
 }
